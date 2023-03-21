43d91d5788aabae1a2aae7d39c9935f588384c72
Ruslan <ruslan.bilorusets@locusenergy.com>
2023-03-20 20:18:33 -0700

    Working prototype

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="healthjoy-cc",
+    version="1.0.0",
+    packages=("healthjoy-cc",),
+    install_requires=(
+        "flask",
+        "requests",
+    ),
+)
