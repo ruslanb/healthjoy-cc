f0ac88375c6905acf21070b4b4d01e9f642fea34
Ruslan <ruslan.bilorusets@locusenergy.com>
2023-03-21 11:41:17 -0700

    Added test for fork-request

--- tests/test_fork.py
+++ tests/test_fork.py
@@ -1,6 +1,14 @@
 
-access_token = 'ghu_e1DnhS4T5GfLtFSYvAUVAkhSHpFjUE0aNmti'
-code = 'd663cf29690cbc5beed8'
+access_token = 'ghu_7JIPTgYCcKfiBuZXWBscTzRdxuZK2w1bSO9p'
+code = '0f6ce141db3195343ced'
+
+
+def test_create_request(client):
+    response = client.get("/fork-request?code={}".format(code))
+    assert response.status_code == 200
+    assert b'Click the button below to fork' in response.data
+    assert b'access_token=' in response.data
+
 
 def test_fork(client):
     response = client.get("/fork-repo?access_token={}".format(access_token))
